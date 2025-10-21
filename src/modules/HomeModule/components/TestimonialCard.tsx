741312948cd945f767b1d4c020e726f1d097c94d
Arulraj V <arulraj@vananam.com>
2025-10-21 14:37:11 +0530

    fix style

--- src/modules/HomeModule/components/TestimonialCard.tsx
+++ src/modules/HomeModule/components/TestimonialCard.tsx
@@ -10,37 +10,34 @@ const TestimonialCard: React.FC<TestimonialCardProps> = ({
   rating,
 }) => {
   return (
-    <div className="bg-white h-[257.28px] w-[343.33px] rounded-[4px] shadow-[0px_10px_31px_0px_rgba(7,152,255,0.09)] relative">
-      {/* Avatar */}
-      <div className="absolute left-[45px] top-[45px] w-[45px] h-[45px] rounded-full shadow-[0px_10px_31px_0px_rgba(7,152,255,0.09)] overflow-hidden">
-        <img src={image} alt={name} className="w-full h-full object-cover" />
-      </div>
+    <div className="bg-white h-[257.28px] w-[343.33px] rounded-[4px] shadow-[0px_10px_31px_0px_rgba(7,152,255,0.09)] p-[45px] flex flex-col">
+      {/* Header: Avatar + Name & Role */}
+      <div className="flex items-center gap-[20px] mb-[26px]">
+        {/* Avatar */}
+        <div className="w-[45px] h-[45px] rounded-full shadow-[0px_10px_31px_0px_rgba(7,152,255,0.09)] overflow-hidden flex-shrink-0">
+          <img src={image} alt={name} className="w-full h-full object-cover" />
+        </div>
 
-      {/* Name */}
-      <div className="absolute left-[110px] top-[60.5px] transform -translate-y-1/2">
-        <h3 className="text-[#141928] text-[18px] font-bold leading-normal whitespace-nowrap">
-          {name}
-        </h3>
-      </div>
-
-      {/* Role */}
-      <div className="absolute left-[110px] top-[82.18px] transform -translate-y-1/2">
-        <p className="text-[#787d8c] text-[14px] leading-[1.5] whitespace-nowrap">
-          {role}
-        </p>
-      </div>
-
-      {/* Testimonial Text Box */}
-      <div className="absolute left-[45px] right-[45px] top-[116px] h-[69.28px] bg-white rounded-[4px]">
-        <div className="absolute left-0 top-1/2 transform -translate-y-1/2">
-          <p className="text-[#787d8c] text-[14px] leading-[1.5]">
-            {testimonial}
+        {/* Name & Role */}
+        <div className="flex flex-col justify-center">
+          <h3 className="text-[#141928] text-[18px] font-bold leading-normal whitespace-nowrap">
+            {name}
+          </h3>
+          <p className="text-[#787d8c] text-[14px] leading-[1.5] whitespace-nowrap">
+            {role}
           </p>
         </div>
       </div>
 
+      {/* Testimonial Text */}
+      <div className="flex-1 mb-[24px]">
+        <p className="text-[#787d8c] text-[14px] leading-[1.5]">
+          {testimonial}
+        </p>
+      </div>
+
       {/* Star Rating */}
-      <div className="absolute left-[45px] top-[185.28px] flex items-center gap-[5.6px] pt-[14px]">
+      <div className="flex items-center gap-[5.6px]">
         {Array.from({ length: rating }).map((_, index) => (
           <SvgStar
             key={index}
